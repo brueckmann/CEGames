95d5f54a429ba0aee7122883dd85d243f48f5fcc
Brueckmann <gracia.brueckmann@unibe.ch>
2025-08-28 14:57:05 +0200

    create shiny app

--- ViewGames/app.R
+++ ViewGames/app.R
@@ -6,56 +6,56 @@
 #
 #    https://shiny.posit.co/
 #
-install.packages("tidyverse")
-
-library(tidyverse)
-
-games <- read_delim("data/ListGames_2025_08_27.csv",
-                                   delim = ";", escape_double = FALSE, na = "NA",
-                                   trim_ws = TRUE)
-
-install.packages("DT")
-
-library(dt)
 
 
-function(input, output) {
+
+games <- readr::read_delim("https://raw.githubusercontent.com/brueckmann/CEGames/refs/heads/main/data/ListGames_2025_08_27.csv",
+                           delim = ";", escape_double = FALSE, na = "NA",
+                           trim_ws = TRUE)
+
+games <- games |>  filter(!((Link == "")))
+
+games <- games |> relocate("Name", "Known serious game")
+
+games <- games |> rename("Serious Game" =  "Known serious game")
+
+
+library(DT)
+library(shiny)
+
+server <- function(input, output) {
 
   # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
     data <- games
-    if (input$`Online Game` != "All") {
-      data <- data[data$`Online Game` == input$`Online Game` ,]
+    if (input$onlineonly != "All") {
+      data <- data[data$`Online Game` == input$onlineonly, ]
     }
-    if (input$`Serious game` != "All") {
-      data <- data[data$`Known serious game` == input$`Known serious game`,]
+    if (input$onlyserious != "All") {
+      data <- data[data$`Known Known serious game` == input$onlyserious, ]
     }
     data
   }))
-
 }
 
 
-
-library(shiny)
-
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  titlePanel("Basic DataTable"),
+  titlePanel("List of Climate and Energy (Serious) Games"),
 
   # Create a new Row in the UI for selectInputs
   fluidRow(
     column(4,
-           selectInput("Online Game",
-                       "Online Game",
+           selectInput("onlineonly",
+                       "Only Online Games?",
                        c("All",
                          unique(as.character(games$`Online Game`))))
     ),
     column(4,
-           selectInput("Serious game",
-                       "Serious game",
+           selectInput("onlyserious",
+                       "Only Known serious games?",
                        c("All",
-                         unique(as.character(games$`Known serious game`))))
+                         unique(as.character(games$`Known Known serious game`))))
     )
   ),
   # Create a new row for the table.
