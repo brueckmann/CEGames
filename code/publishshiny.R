dc874bef49344c5db664adac85af226bca58a30e
Brueckmann <gracia.brueckmann@unibe.ch>
2025-09-19 19:08:36 +0200

    try automatic with favicon

--- code/publishshiny.R
+++ code/publishshiny.R
@@ -1,7 +1,9 @@
 
 # shinylive::export(appdir = "./Shiny/ViewGames/", destdir = "og_docs")
 
-shinylive::export(appdir = "./Shiny/ViewGamesShiny/", destdir = "docs")
+shinylive::export(appdir = "./Shiny/ViewGamesShiny/",
+                  destdir = "docs",
+                  template_dir = "docs/CEGamesShinylifetemplate")
 
 
-httpuv::runStaticServer("docs/", port = 8008)
+# httpuv::runStaticServer("docs/", port = 8008)
